d7ddfcfbbd3b844673e7bd7c5e3bf0aad652d87d
Dejan Popov <dekipopica92@gmail.com>
2025-09-16 17:28:55 +0200

    Ship moving with W,A,S,D keys

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,18 +2,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float controlSpeed = 50f;
+    
+    private Vector2 _movement;
+    
     void Update()
     {
-        
+        ProcessTranslation();
     }
-
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        _movement = (value.Get<Vector2>());
     }
+    
+    private void ProcessTranslation()
+    {
+        float xOffset = _movement.x * controlSpeed * Time.deltaTime;
+        float yOffset = _movement.y * controlSpeed * Time.deltaTime;
+        
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, 
+                                              transform.localPosition.y + yOffset, 
+                                              0f);
+    }
+
+
 }

20014db52dd7bc4d1eff880ba9b182bd3d651571
Dejan Popov <dekipopica92@gmail.com>
2025-09-17 19:59:11 +0200

    Using Matf.Clamp() so ship cant go offscreen, setting playing boundries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed = 50f;
+    [SerializeField] private float xClampRange  = 30f;
+    [SerializeField] private float yClampRange  = 30f;
     
     private Vector2 _movement;
     
@@ -19,11 +22,13 @@ public class PlayerMovement : MonoBehaviour
     {
         float xOffset = _movement.x * controlSpeed * Time.deltaTime;
         float yOffset = _movement.y * controlSpeed * Time.deltaTime;
+
+        float rawXPOS = transform.localPosition.x + xOffset;
+        float rawYPOS = transform.localPosition.y + yOffset;
+
+        float clampedXPOS = Mathf.Clamp(rawXPOS, -xClampRange, xClampRange);
+        float clampedYPOS = Mathf.Clamp(rawYPOS, -yClampRange, yClampRange);
         
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, 
-                                              transform.localPosition.y + yOffset, 
-                                              0f);
+        transform.localPosition = new Vector3(clampedXPOS,clampedYPOS,0f);
     }
-
-
 }
